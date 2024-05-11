1bb7900b95485fee49092a99239283f082f55dd8
MohammadShahheydar <m.shahheydar110>
2024-05-12 00:53:43 +0330

    tools ui complete

--- src/component/landing/OfferingSectionV2.tsx
+++ src/component/landing/OfferingSectionV2.tsx
@@ -3,6 +3,9 @@ import { OFFERING_SECTION } from "@/constatns/landingPage.constants";
 import { GradientShape } from "@/component/general/GradientShape";
 import Link from "next/link";
 import ArrowRight from "@/icons/ArrowRight";
+import QuoteIcon from "@/icons/QuoteIcon";
+import Image from "next/image";
+import ToolItem from "@/component/landing/tools/ToolItem";
 
 const OfferingSectionV2 = () => {
   return (
@@ -11,17 +14,38 @@ const OfferingSectionV2 = () => {
         <div className='text-3xl md:text-4xl lg:text-6xl font-semibold'>{OFFERING_SECTION.title}</div>
         {/*<div className='text-md md:text-2xl'>{OFFERING_SECTION.description}</div>*/}
       </div>
-      <div className='flex flex-col gap-10 relative z-20 w-full'>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-stretch justify-between w-full">
-          <div className="">
-            <h3 className={'font-extrabold text-4xl mb-5'}>LevelSurf</h3>
-            <p className={'text-xl font-light mb-5'}>Provides real-time visualization and alerts for major support and resistance and other key market levels,
-              aiding traders in identifying critical market thresholds.</p>
-            <Link className={"transition-all flex items-center gap-1 hover:underline underline-offset-4"} href={"#"}><p className={'text-xl'}>LEARN MORE </p><ArrowRight className={'hidden'}/></Link>
-            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-          </div>
-        </div>
-        <div className='absolute -z-10 top-[20%] left-[50%] -translate-x-[50%] w-full scale-[400%]'>
+      <div className='flex flex-col gap-36 relative z-20 w-full items-center'>
+        <ToolItem
+          direction={'left'}
+          image={'/images/LevelSurf-1.png'}
+          imageAlt={"LevelSurf"}
+          title={"LevelSurf"}
+          description={"Provides real-time visualization and alerts for major support and resistance and other key market levels, aiding traders in identifying critical market thresholds."}
+          quote={"“By automating the majority of steps related to patient follow-ups, Rad AI removes those manual tasks from our clinical team and gives them back more time to focus on caring for their patients.”"}
+          user={'/images/aaron korbs.jpg'}
+          userAlt={'aaron korbs'}
+          userCompany={'/images/company.png'}
+          userCompanyAlt={'company'}
+          link={"#"}
+          userJob={"aaron korbs position"}
+          userName={"aaron korbs"}
+        />
+        <ToolItem
+          direction={'right'}
+          image={'/images/LevelSurf-1.png'}
+          imageAlt={"LevelSurf"}
+          title={"LevelSurf"}
+          description={"Provides real-time visualization and alerts for major support and resistance and other key market levels, aiding traders in identifying critical market thresholds."}
+          quote={"“By automating the majority of steps related to patient follow-ups, Rad AI removes those manual tasks from our clinical team and gives them back more time to focus on caring for their patients.”"}
+          user={'/images/aaron korbs.jpg'}
+          userAlt={'aaron korbs'}
+          userCompany={'/images/company.png'}
+          userCompanyAlt={'company'}
+          link={"#"}
+          userJob={"aaron korbs position"}
+          userName={"aaron korbs"}
+        />
+        <div className="absolute -z-10 top-[20%] left-[50%] -translate-x-[50%] w-full scale-[400%]">
           <GradientShape />
         </div>
       </div>
--- src/component/tools/Tools.tsx
+++ src/component/tools/Tools.tsx
@@ -1,5 +1,5 @@
 import {useRouter} from "next/router";
-import {TOOLS} from "@/constatns/tools.constants";
+// import {TOOLS} from "@/constatns/tools.constants";
 
 
 const Tools = (
@@ -18,12 +18,12 @@ const Tools = (
       <div className='flex flex-col gap-2'>
         <div className='flex flex-col gap-1'>
           <div className='pageTitle w-full'>
-            <h1 className={'text-3xl md:text-4xl 2xl:text-5xl text-center'}>{TOOLS[params.tool]?.title}</h1>
+            {/*<h1 className={'text-3xl md:text-4xl 2xl:text-5xl text-center'}>{TOOLS[params.tool]?.title}</h1>*/}
           </div>
         </div>
       </div>
       <div className="mt-6 text-center">
-        <p className={'text-xl'}>{TOOLS[params.tool]?.description}</p>
+        {/*<p className={'text-xl'}>{TOOLS[params.tool]?.description}</p>*/}
       </div>
     </div>
   );
