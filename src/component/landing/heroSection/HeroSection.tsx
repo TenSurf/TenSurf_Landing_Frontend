39ad73d1a3f47ba2c5e6cdefa0590f17808312f0
MohammadShahheydar <m.shahheydar110>
2024-05-14 00:48:02 +0330

    video -> responsive

--- src/component/landing/heroSection/HeroSection.tsx
+++ src/component/landing/heroSection/HeroSection.tsx
@@ -27,27 +27,27 @@ export const HeroSection: FC<IProps> = ({
   const animateLights = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <div
-      style={{ height: Number(viewPortHeight) - Number(headerHeight) + "px" }}
-      className="relative w-full flex flex-col items-center justify-center overflow-hidden"
+      className={`relative w-full flex flex-col items-center overflow-hidden h-screen`}
     >
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 relative z-[9] max-w-[1700px] mt-5">
-        <div className="flex gap-14 flex-col items-start text-center bottom-6 max-w-screen-xl">
+      <div className="grid grid-cols-1 lg:grid-cols-2 justify-center gap-14 lg:gap-20 relative z-[9] max-w-[1700px] mt-[2vh] lg:mt-[15vh]">
+        <div className="flex gap-8 lg:gap-14 flex-col items-center lg:items-start text-center bottom-6 max-w-screen-xl mt-10 lg:mt-0">
           <h1 className="text-primaryGradient text-left staticPageTitle">
             {HERO_SECTION.title}
           </h1>
-          <ul className={"w-full px-6 list-disc text-left"}>
-            {HERO_SECTION.items.map((val) => (
-              <li key={val}>
-                <h4 className={"font-light text-2xl lg:text-3xl"}>{val}</h4>
-              </li>
-            ))}
-          </ul>
+          <p className={'text-center lg:text-left font-light text-2xl lg:text-3xl'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores at, blanditiis, cum deleniti enim eos incidunt iste laboriosam minima natus,</p>
+          {/*<ul className={"w-full px-6 list-disc text-left"}>*/}
+          {/*  {HERO_SECTION.items.map((val) => (*/}
+          {/*    <li key={val}>*/}
+          {/*      <h4 className={"font-light text-2xl lg:text-3xl"}>{val}</h4>*/}
+          {/*    </li>*/}
+          {/*  ))}*/}
+          {/*</ul>*/}
           {/*<h2 className='text-md md:text-2xl font-semibold mt-4 mb-6 leading-6 md:leading-8'>*/}
           {/*  {HERO_SECTION.description}*/}
           {/*</h2>*/}
-          <div className="font-bold md:font-extrabold text-lg md:text-xl">
-            {HERO_SECTION.description2}
-          </div>
+          {/*<div className="font-bold md:font-extrabold text-lg md:text-xl">*/}
+          {/*  {HERO_SECTION.description2}*/}
+          {/*</div>*/}
           <a href={process.env.NEXT_PUBLIC_HUB_URL}>
             <Button
               // onClick={
@@ -66,17 +66,17 @@ export const HeroSection: FC<IProps> = ({
             </Button>
           </a>
         </div>
-        <div className={"relative rounded overflow-hidden h-fit"}>
+        <div className={"relative rounded overflow-hidden h-fit mx-auto sm:max-w-2xl lg:max-w-none"}>
           <div
             className="cursor-pointer bg-black z-[11] absolute w-full h-full opacity-70 backdrop-blur flex items-center justify-center">
             <Dialog open={videoModal} modal={videoModal} onOpenChange={setVideoModal}>
               <DialogTrigger asChild>
                 <Play onClick={() => setVideoModal(true)} size={64} className={"stroke-[#8f8f8f] hover:stroke-[#ffffff]"} strokeWidth={6} />
               </DialogTrigger>
-              <DialogContent className={'w-full sm:max-w-2xl'}>
-                <div className="w-full h-full">
+              <DialogContent className={'w-[80vw] max-w-7xl'}>
+                <div className="w-full h-full p-2">
                   <video controls loop playsInline width={"100%"} height={"100%"}>
-                    <source src={"/video/dummy_video.mp4"} type="video/mp4" />
+                    <source src={"/video/TenSurf Hub Demo Website.mp4"} type="video/mp4" />
                   </video>
                 </div>
               </DialogContent>
@@ -92,7 +92,7 @@ export const HeroSection: FC<IProps> = ({
         onClick={() => {
           productsRef?.current?.scrollIntoView({ behavior: "smooth" });
         }}
-        className="absolute z-10 bottom-8 h-8 w-8 animate-bounce cursor-pointer"
+        className="absolute z-10 bottom-24 h-8 w-8 animate-bounce cursor-pointer"
       />
 
       <div
