209a10a175d56adc6910f786564ba4c9c3a757ff
MohammadShahheydar <m.shahheydar110>
2024-05-15 23:22:13 +0330

    add video with initial loading

--- src/component/landing/heroSection/HeroSection.tsx
+++ src/component/landing/heroSection/HeroSection.tsx
@@ -1,14 +1,12 @@
 import { HERO_SECTION } from "../../../constatns/landingPage.constants";
 import ChevronDownIcon from "../../../icons/ChevronDownIcon";
-import React, { type FC, type RefObject, useEffect, useState } from "react";
+import React, { type FC, type RefObject, useEffect, useRef, useState } from "react";
 import { AnimateLight } from "./AnimateLight";
 // import { useRouter } from 'next/router';
 import { Button } from "@/components/ui/button";
 import { Play } from "lucide-react";
 import AOS from "aos";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 
 interface IProps {
   viewPortHeight?: number;
@@ -22,6 +20,9 @@ export const HeroSection: FC<IProps> = (
     headerHeight,
     productsRef
   }) => {
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [videoLoading, setVideoLoading] = useState<"idl" | "pend" | "full">("idl");
 
   let router = useRouter();
 
@@ -87,15 +88,38 @@ export const HeroSection: FC<IProps> = (
           </a>
         </div>
         <div
-          className={"relative rounded-2xl overflow-hidden h-fit mx-auto sm:max-w-2xl lg:max-w-none pb-[67%] w-full"}>
+          className={"relative rounded-2xl overflow-hidden h-fit mx-auto sm:max-w-2xl lg:max-w-none w-full"}>
           <div
-            className="cursor-pointer bg-black z-[11] absolute w-full h-full opacity-30 backdrop-blur flex items-center justify-center">
-            <Link target={'_blank'} href={`${process.env.NEXT_PUBLIC_HUB_URL}/videos/tensurf-demo.mp4`}>
-              <Play size={64} className={"stroke-[#8f8f8f] hover:stroke-[#ffffff]"}
-                    strokeWidth={6} />
-            </Link>
+            className={`${videoLoading == "full" && "hidden"} cursor-pointer bg-black z-[11] absolute w-full h-full opacity-30 backdrop-blur flex items-center justify-center`}>
+            {videoLoading == "pend" ? (
+              <div role="status">
+                <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                     viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor" />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill" />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
+            ) : (
+              <>
+                <Play onClick={async () => {
+                  setVideoLoading("pend");
+                  await videoRef?.current?.play();
+                  setVideoLoading("full");
+                }} size={64} className={"stroke-[#8f8f8f] hover:stroke-[#ffffff]"}
+                      strokeWidth={6} />
+              </>
+            )}
+
           </div>
-          <Image src={"/images/video-preview.png"} alt={"landing video"} layout={"fill"} />
+          <video ref={videoRef} poster={"/images/video-preview.png"} muted loop playsInline width={"100%"}
+                 height={"100%"}>
+            <source src={`${process.env.NEXT_PUBLIC_HUB_URL}/videos/tensurf-demo.mp4`} type="video/mp4" />
+          </video>
         </div>
       </div>
 
