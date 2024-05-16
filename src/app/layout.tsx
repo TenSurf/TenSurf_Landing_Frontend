b9ca3aade6c20cad68421eb1a8263be443caa3f1
MohammadShahheydar <m.shahheydar110>
2024-05-16 09:41:41 +0330

    add hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,18 @@ export default function RootLayout({
         gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALIYTICS}');` }}>
 
       </Script>
+      <Script
+        id={"hotjar"} strategy={"afterInteractive"}
+              dangerouslySetInnerHTML={{ __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:4976909,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');` }}>
+
+      </Script>
     </head>
     <body className={inter.className}>
     <ThemeProvider
