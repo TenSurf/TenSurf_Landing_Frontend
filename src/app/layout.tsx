922a61827283b015a0807a5c6199b41ea5a5d6cf
h.ahari <hsoleimaneahary@gmail.com>
2024-05-17 10:38:47 +0330

    add hotjar script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,12 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ToastContainer } from "react-toastify";
 import { Toaster } from "@/components/ui/sonner";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tensurf",
+  title: "TenSurf",
   description:
     'Revolutionizing AI Trading\n"At TenSurf, we are committed to revolutionizing AI trading and democratizing algorithmic trading. Our mission is to harness the power of Generative AI and Data Science to enable smart trading decisions."',
   icons: ["/favicon.ico", "/logo.png"],
@@ -21,6 +22,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALIYTICS}`}
+        ></Script>
+        <Script
+          id={"google-tag-manager"}
+          strategy={"afterInteractive"}
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALIYTICS}');`,
+          }}
+        ></Script>
+        <Script
+          id={"hotjar"}
+          strategy={"afterInteractive"}
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:4985901,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+          }}
+        ></Script>
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
