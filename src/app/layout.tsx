566d2445cc3f5f4f738c178866194aaa1081b6e6
MohammadShahheydar <m.shahheydar110>
2024-05-20 00:22:35 +0330

    add mouseflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,59 +11,72 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "TenSurf",
   description:
-    'Revolutionizing AI Trading\n"At TenSurf, we are committed to revolutionizing AI trading and democratizing algorithmic trading. Our mission is to harness the power of Generative AI and Data Science to enable smart trading decisions."',
-  icons: ["/favicon.ico", "/logo.png"],
+    "Revolutionizing AI Trading\n\"At TenSurf, we are committed to revolutionizing AI trading and democratizing algorithmic trading. Our mission is to harness the power of Generative AI and Data Science to enable smart trading decisions.\"",
+  icons: ["/favicon.ico", "/logo.png"]
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <Script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALIYTICS}`}
-        ></Script>
-        <Script
-          id={"google-tag-manager"}
-          strategy={"afterInteractive"}
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
+    <head>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALIYTICS}`}
+      ></Script>
+      <Script
+        id={"google-tag-manager"}
+        strategy={"afterInteractive"}
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALIYTICS}');`,
-          }}
-        ></Script>
-        <Script
-          id={"hotjar"}
-          strategy={"afterInteractive"}
-          dangerouslySetInnerHTML={{
-            __html: `(function(h,o,t,j,a,r){
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALIYTICS}');`
+        }}
+      ></Script>
+      <Script
+        id={"hotjar"}
+        strategy={"afterInteractive"}
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
                 h._hjSettings={hjid:4985901,hjsv:6};
                 a=o.getElementsByTagName('head')[0];
                 r=o.createElement('script');r.async=1;
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                 a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-          }}
-        ></Script>
-      </head>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <App />
-          {children}
-        </ThemeProvider>
-        <Toaster duration={3000} />
-      </body>
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+        }}
+      ></Script>
+      <Script
+        id={"mouseflow"}
+        strategy={"afterInteractive"}
+        dangerouslySetInnerHTML={{
+          __html: `window._mfq = window._mfq || [];
+                (function() {
+                  var mf = document.createElement("script");
+                  mf.type = "text/javascript"; mf.defer = true;
+                  mf.src = "//cdn.mouseflow.com/projects/9bc15f72-9b4a-4ee7-b0a1-84ce917ca48f.js";
+                  document.getElementsByTagName("head")[0].appendChild(mf);
+                  })();`
+        }}
+      ></Script>
+    </head>
+    <body className={inter.className}>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <App />
+      {children}
+    </ThemeProvider>
+    <Toaster duration={3000} />
+    </body>
     </html>
   );
 }
