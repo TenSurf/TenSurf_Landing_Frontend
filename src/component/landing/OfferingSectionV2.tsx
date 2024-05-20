e1b5c533a6a0f6c2bc017eb4a30f35670422306c
MohammadShahheydar <m.shahheydar110>
2024-05-21 01:28:35 +0330

    lading page tools complete

--- src/component/landing/OfferingSectionV2.tsx
+++ src/component/landing/OfferingSectionV2.tsx
@@ -17,10 +17,11 @@ const OfferingSectionV2 = () => {
       <div className='flex flex-col gap-36 relative z-20 w-full items-center'>
         <ToolItem
           direction={'left'}
+          name={'TenSurf Brain'}
           image={'/images/LevelSurf-1.png'}
           imageAlt={"LevelSurf"}
-          title={"LevelSurf"}
-          description={"Provides real-time visualization and alerts for major support and resistance and other key market levels, aiding traders in identifying critical market thresholds."}
+          title={"Advanced Trading Insights by chatting with TenSurf Brain"}
+          description={"TenSurf Brain is an artificial intelligence-powered chatbot designed to provide traders with advanced tools for enhancing their trading strategies. You can interact with this AI chatbot via both voice and text chat to access features such as trend detection and support/resistance analysis, which help you visually identify key price levels on charts for informed entry and exit decisions. Additionally, TenSurf Brain calculates precise stop loss and take profit levels to aid in robust risk management. It also evaluates trading biases to recommend whether to look for long or short positions, giving you strategic insights into various financial instruments. Plus, you can give YouTube videos directly to TenSurf Brain to receive concise summaries of the trading strategies discussed, further enhancing your learning and application of new trading techniques."}
           quote={"“By automating the majority of steps related to patient follow-ups, Rad AI removes those manual tasks from our clinical team and gives them back more time to focus on caring for their patients.”"}
           user={'/images/aaron korbs.jpg'}
           userAlt={'Aaron Korbs'}
@@ -32,10 +33,27 @@ const OfferingSectionV2 = () => {
         />
         <ToolItem
           direction={'right'}
+          name={'ChartSurf '}
           image={'/images/LevelSurf-1.png'}
           imageAlt={"LevelSurf"}
-          title={"LevelSurf"}
-          description={"Provides real-time visualization and alerts for major support and resistance and other key market levels, aiding traders in identifying critical market thresholds."}
+          title={"Automated Key Levels and Event Insights in ChartSurf"}
+          description={"ChartSurf is an advanced charting tool within TenSurf Hub that incorporates a range of analytical features to support effective trading. It includes tools for identifying delta and footprint imbalances, trend analysis, and displaying major order flow levels for various assets. It also provides insights into key market levels like initial balance, previous session highs/lows, and overnight session extremes, alongside volume profile and VWAP levels to pinpoint key trading levels. Additionally, ChartSurf automatically calculates and visualizes support/resistance (S/R) levels, using our proprietary machine learning algorithm, to aid traders in making strategic decisions regarding entry and exit points. This S/R tool identifies and visualizes key support and resistance levels where prices historically reverse or stall, helping traders determine entry and exit points and assess trend strength. Furthermore, ChartSurf features automatic display of price breaking the key levels or holding them based on market typical rotation size, offering a comprehensive solution for analyzing and responding to market rapid dynamics."}
+          quote={"“By automating the majority of steps related to patient follow-ups, Rad AI removes those manual tasks from our clinical team and gives them back more time to focus on caring for their patients.”"}
+          user={'/images/aaron korbs.jpg'}
+          userAlt={'Aaron Korbs'}
+          userCompany={'/images/company.png'}
+          userCompanyAlt={'company'}
+          link={"#"}
+          userJob={"Aaron Korbs position"}
+          userName={"Aaron Korbs"}
+        />
+        <ToolItem
+          direction={'left'}
+          name={'WidgetSurf  '}
+          image={'/images/LevelSurf-1.png'}
+          imageAlt={"LevelSurf"}
+          title={"Data-driven Insights with WidgetSurf"}
+          description={"WidgetSurf integrates a diverse range of widgets for comprehensive market analysis and trading strategy optimization. It includes TrendSurf for tracking short-term and long-term trends, GapSurf for analyzing price gaps, WaveSurf for getting typical market volatility measures, PairSurf for understanding market correlations, LiqSurf for order book analysis, TimeSurf for analyzing market behavior around news events, and StatSurf for statistical analysis of price events on key levels. Especifically, StatSurf provides insights into the probability of breaking or retesting key market levels based on the scenario present in the current session, utilizing historical data. These tools collectively empower traders with valuable insights and data-driven decision-making capabilities."}
           quote={"“By automating the majority of steps related to patient follow-ups, Rad AI removes those manual tasks from our clinical team and gives them back more time to focus on caring for their patients.”"}
           user={'/images/aaron korbs.jpg'}
           userAlt={'Aaron Korbs'}
--- src/component/landing/tools/ToolItem.tsx
+++ src/component/landing/tools/ToolItem.tsx
@@ -1,5 +1,5 @@
 import "aos/dist/aos.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import ArrowRight from "@/icons/ArrowRight";
 import QuoteIcon from "@/icons/QuoteIcon";
@@ -18,13 +18,16 @@ const ToolItem = (
     userCompany,
     quote,
     description,
-    userJob, userName,
-    title
+    userJob,
+    userName,
+    title,
+    name
   }: {
     direction: "left" | "right",
     image: string,
     imageAlt: string,
     title: string,
+    name: string,
     description: string,
     quote: string,
     user: string,
@@ -36,32 +39,39 @@ const ToolItem = (
     link: string
   }) => {
 
+  const descriptionRef = useRef<HTMLParagraphElement>(null);
+  const [paragraphHeight, setParagraphHeight] = useState<number>();
+
   useEffect(() => {
     AOS.init({
       duration: 2000,
       anchorPlacement: 'center-center',
       once: true,
     });
+
+    setParagraphHeight(descriptionRef.current?.clientHeight)
+
   }, [])
 
 
   return (
     <div
-      className="grid grid-cols-1 lg:grid-cols-2 md:max-w-[600px] lg:max-w-none px-6 sm:px-0 gap-14 items-start justify-between w-full">
+      className="grid grid-cols-1 lg:grid-cols-2 md:max-w-[600px] lg:max-w-none px-6 sm:px-0 gap-8 items-start justify-between w-full">
       <div data-aos={direction == "right" ? 'fade-left' : 'fade-right'} className={direction == "left" ? "order-1" : "order-2"}>
-        <h3 className={"font-extrabold text-4xl mb-5"}>{title}</h3>
-        <p className={"text-xl font-light mb-10"}>{description}</p>
-        <Link className={"group transition-all flex items-center gap-1 hover:underline underline-offset-4"} href={link}>
+        <h6 className={"font-extrabold text-xl mb-1 text-primaryGradient"}>{name}</h6>
+        <h3 className={"font-bold text-3xl leading-10 mb-3"}>{title}</h3>
+        <p ref={descriptionRef} className={`cursor-pointer overflow-hidden max-h-[${paragraphHeight ?? 0}px] text-lg font-light mb-2 line-clamp-3 hover:line-clamp-[40] duration-1000 hover:max-h-[1000px] transition-all`}>{description}</p>
+        <Link className={"group transition-all flex items-center gap-1 hover:underline underline-offset-4 w-fit"} href={link}>
           <p className={"text-xl"}>LEARN MORE </p>
           <ArrowRight className={"transition-all group-hover:translate-x-1/4 "} />
         </Link>
-        <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-        <div className="flex items-start gap-4">
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700" />
+        <div className="flex items-start gap-3">
           <div>
             <QuoteIcon className={"w-[32px] h-[32px]"} />
           </div>
-          <div className="flex-1 flex flex-col gap-4">
-            <p>{quote}</p>
+          <div className="flex-1 flex flex-col gap-2">
+            <p className={''}>{quote}</p>
             <div className="flex justify-between items-center">
               <div className="flex gap-4 items-stretch">
                 <Image objectFit={"contain"} src={user} alt={userAlt} width={60}
