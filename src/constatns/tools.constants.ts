b831290c94358d14a16ecd84c32e599aa2ac12d5
MohammadShahheydar <m.shahheydar110>
2024-05-21 08:15:40 +0330

    tools not complete

---
+++ src/constatns/tools.constants.ts
@@ -0,0 +1,94 @@
+export const tools = {
+  "WidgetSurf": {
+    title: "WidgetSurf",
+    header: "Enhanced Trading Insights with WidgetSurf in TenSurf Hub",
+    description: "WidgetSurf in TenSurf Hub integrates a diverse range of tools for comprehensive market analysis and trading strategy optimization. These tools collectively empower traders with valuable insights and data-driven decision-making capabilities.",
+    detailDescriptionTitle: "Detailed Descriptions of WidgetSurf Tools",
+    items: [
+      {
+        title: "TrendSurf",
+        description: "TrendSurf offers comprehensive market trend analysis, showcasing both short-term and long-term trends.",
+        list: [
+          {
+            title: "Short-term trends",
+            description: "Includes trends from the past hour, the current trading session, and overnight activities"
+          },
+          {
+            title: "Long-term trends",
+            description: "Covers trends from the previous session, the past week, and the past month. This capability enables users to simultaneously understand the short-term and long-term trends of the market accurately."
+          }
+        ]
+      },
+      {
+        title: "GapSurf",
+        description: "GapSurf focuses on identifying and analyzing price gaps in the market.",
+        list: [
+          {
+            title: "Price Gap Analysis",
+            description: "Tracks market price gaps and offers statistical insights on the likelihood of these gaps being filled."
+          },
+          {
+            title: "Opportunity Identification",
+            description: "Lists instruments that have opened with a gap and monitors their progress towards closing the gap. This tool assists traders in spotting potential price movements and capitalizing on gap-related trading opportunities."
+          }
+        ]
+      },
+      {
+        title: "WaveSurf",
+        description: "WaveSurf calculates market volatility using two key indicators:",
+        list: [
+          {
+            title: "Zigzag Indicator",
+            description: "Utilizes a proprietary algorithm that requires a 0.2% price change and at least three candles per leg across different sessions."
+          },
+          {
+            title: "ATR (Average True Range) Indicator",
+            description: "Measures short-term volatility by calculating the average range between high and low prices over 1-minute intervals. These indicators help traders assess the market's volatility patterns, aiding in the prediction of significant price movements."
+          }
+        ]
+      },
+      {
+        title: "PairSurf",
+        description: "PairSurf aids in understanding market correlations and comparative trends between different trading instruments.",
+        list: [
+          {
+            title: "Market Correlations",
+            description: "Helps traders analyze the relationships between various assets."
+          },
+          {
+            title: "Comparative Trends",
+            description: "Useful for pairs trading by highlighting how different instruments move in relation to each other. This tool is essential for traders who want to leverage the interdependencies between markets to inform their trading strategies."
+          }
+        ]
+      },
+      {
+        title: "LiqSurf",
+        description: "LiqSurf focuses on analyzing the order book to identify market pressure.",
+        list: [
+          {
+            title: "Order Book Ratio",
+            description: " Calculates the dominant side of the order book by summing bid and ask liquidity levels."
+          },
+          {
+            title: "Calculation Methods",
+            description: "Includes fixed percentage ranges, zigzag patterns based on price changes, and ATR analysis on 1-minute timeframes. This functionality provides insights into market pressure exerted by buyers or sellers, which can indicate potential market direction changes."
+          }
+        ]
+      },
+      {
+        title: "TimeSurf",
+        description: "TimeSurf analyzes market behavior in relation to news events.",
+        list: [
+          {
+            title: "News Event Analysis",
+            description: "Evaluates how specific news events impact market behavior."
+          },
+          {
+            title: "Optimal Trading Times",
+            description: "Highlights periods of potential high volatility or stability for strategic planning. This tool is especially useful for traders planning their strategies around news releases and other time-sensitive events that could affect market conditions."
+          }
+        ]
+      },
+    ]
+  }
+}
