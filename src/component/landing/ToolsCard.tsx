db748c7b3d1b5f4d7f478a550e1654335ac8cd8f
MohammadShahheydar <m.shahheydar110>
2024-06-10 02:25:49 +0330

    update landing

--- src/component/landing/ToolsCard.tsx
+++ src/component/landing/ToolsCard.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ChatbotIcon from "@/icons/ChatbotIcon";
 import ToolCard from "@/component/landing/ToolCard";
+import { GradientShape } from "@/component/general/GradientShape";
 
 const ToolsCard = () => {
   return (
-    <div className="relative flex flex-col justify-center items-center gap-16 scroll-m-28 max-w-screen-2xl w-full">
-      <div className={'flex flex-col items-center justify-center gap-12'}>
+    <div
+      className=" relative overflow-visible flex flex-col justify-center items-center gap-16 scroll-m-28 max-w-screen-2xl w-full">
+      {/*<div className="absolute tools-radial-gradient inset-0 w-full h-[180px] z-[0]"></div>*/}
+      <div className={'z-10 flex flex-col items-center justify-center gap-12'}>
         <div className="bg-[#00041A] rounded-full p-2 flex gap-4 justify-between items-center">
           <div className="rounded-full p-3 bg-primary">
             <ChatbotIcon />
@@ -18,20 +21,34 @@ const ToolsCard = () => {
             Institutional-Grade Trading Tools</p>
         </div>
       </div>
-      <div className="w-full flex flex-col gap-4">
+      <div className="z-10 w-full flex flex-col gap-4">
         <div className="w-full grid md:grid-cols-2 xl:grid-cols-3 grid-rows-1 gap-4 items-stretch">
-          <ToolCard title={'Advanced Insights'} description={'Identify patterns, imbalances, anomalies, level reactions, statistics and limit order book directly on chart.'}/>
-          <ToolCard title={'Strategy Automation'} description={'Automate and test trading strategies with ease by chatting with AI agents without any need for coding.'}/>
-          <ToolCard className={'md:col-span-2 xl:col-span-1'} title={'Risk Manager'} description={'Prevent emotional or revenge trading by defining guardrails for your trading rules in a no-code chat interface.'}/>
+          <ToolCard title={'Advanced Insights'}
+                    description={'Identify patterns, imbalances, anomalies, level reactions, statistics and limit order book directly on chart.'} />
+          <ToolCard title={'Strategy Automation'}
+                    description={'Automate and test trading strategies with ease by chatting with AI agents without any need for coding.'} />
+          <ToolCard className={'md:col-span-2 xl:col-span-1'} title={'Risk Manager'}
+                    description={'Prevent emotional or revenge trading by defining guardrails for your trading rules in a no-code chat interface.'} />
         </div>
         <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
-          <ToolCard className={''} title={'Conversational AI Assistants'} description={'Chat with the Market: Engage with the conversational interface to get real-time market analysis and high-impact news analysis on the chart. History Analysis: Utilize text or voice chat to analyze your trading patterns, chat with previous trades to gain insights, and pinpoint areas for improvement and untapped opportunities. Smart Agents: Ignite AI agents via text or voice chat to monitor market conditions and identify the best trade opportunities.'}/>
+          <ToolCard className={''} title={'Conversational AI Assistants'}
+                    description={'Chat with the Market: Engage with the conversational interface to get real-time market analysis and high-impact news analysis on the chart. History Analysis: Utilize text or voice chat to analyze your trading patterns, chat with previous trades to gain insights, and pinpoint areas for improvement and untapped opportunities. Smart Agents: Ignite AI agents via text or voice chat to monitor market conditions and identify the best trade opportunities.'} />
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1 gap-4">
-            <ToolCard title={"Psychological Profiling"} description={"Receive alerts and reports for prevalent trading pitfalls based on speech analysis and facial emotion detection in real-time."}/>
-            <ToolCard title={"Investment Advice"} description={"Stock analysis and investment suggestions by AI researchers based on EDGAR filings, financial news, and market trends."}/>
+            <ToolCard title={"Psychological Profiling"}
+                      description={"Receive alerts and reports for prevalent trading pitfalls based on speech analysis and facial emotion detection in real-time."} />
+            <ToolCard title={"Investment Advice"}
+                      description={"Stock analysis and investment suggestions by AI researchers based on EDGAR filings, financial news, and market trends."} />
           </div>
         </div>
       </div>
+      <div
+        style={{
+          opacity: '0.8',
+          background: 'radial-gradient(circle, rgba(37,97,255,0.5) 0%, rgba(1,3,11,1) 100%)',
+          filter: 'blur(200px)'
+        }}
+        className='z-0 absolute w-[500px] top-0 h-[500px] rounded-full'
+      ></div>
     </div>
   );
 };
