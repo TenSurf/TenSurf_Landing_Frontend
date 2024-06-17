1f360206d075c38152b9e39ca6d7b61f8af5dcd6
MohammadShahheydar <m.shahheydar110>
2024-06-17 21:23:58 +0330

    init gsap animation

---
+++ src/utils/GsapAnimation.tsx
@@ -0,0 +1,18 @@
+"use client"
+import React from "react";
+import { useGSAP } from "@gsap/react";
+
+const GsapAnimation = () => {
+
+  useGSAP(() => {
+
+  } , {})
+
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default GsapAnimation;
