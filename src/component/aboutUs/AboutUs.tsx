81b020a360f21bd5c82ed006a1c7b59c5f94edf5
MohammadShahheydar <m.shahheydar110>
2024-06-18 23:09:25 +0330

    about us complete

--- src/component/aboutUs/AboutUs.tsx
+++ src/component/aboutUs/AboutUs.tsx
@@ -1,7 +1,6 @@
 // *.*.*.*.*.*.*.*.*.*.* Main Function ↓•↓•↓
 import React from "react";
 import Image from "next/image";
-import AboutUsIcon from "@/icons/AboutUsIcon";
 import AboutUsItem from "@/component/aboutUs/AboutUsItem";
 
 export const AboutUs = () => {
@@ -14,10 +13,30 @@ export const AboutUs = () => {
         <div
           className="absolute w-screen bg-[url('/images/subtitleBackground.svg')] bg-no-repeat bg-cover h-[200vh] bottom-0">
         </div>
-        <Image src={"/images/about-us.png"} className={'2xl:rounded-3xl max-w-screen-2xl mx-auto'} objectFit={"cover"} alt={"About Us"} fill />
+        <Image src={"/images/about-us.png"} className={"2xl:rounded-3xl max-w-screen-2xl mx-auto"} objectFit={"cover"}
+               alt={"About Us"} fill />
       </div>
       <div className="container w-full mx-auto flex-col md:gap-[200px]">
-        <AboutUsItem/>
+        <AboutUsItem
+          className={'mb-48 lg:mb-96'}
+          title={"Born from Passion, Driven by Innovation"}
+          description={"We built a cutting-edge trading platform powered by TenSurf Brain, a suite of AI, ML, and data science tools. This translates to sophisticated analytics, robust risk management, and personalized assistance – all designed to elevate your trading experience."}
+          direction={"left"} image={"/images/about-us-1.png"}
+        />
+        <AboutUsItem
+          className={'mb-48 lg:mb-96'}
+          title={"TenSurf Brain: Your AI Advantage"}
+          description={"TenSurf was founded by a team of industry veterans and technology enthusiasts with a shared vision (empower traders of all levels with the tools and insights needed to thrive in today markets)."}
+          direction={"right"}
+          image={"/images/about-us-2.png"}
+        />
+        <AboutUsItem
+          className={'mb-16'}
+          title={"Democratizing Trading"}
+          description={"We believe powerful trading tools shouldn be a privilege for the few. TenSurf mission is to level the playing field, providing everyone with the resources they need to succeed."}
+          direction={"left"}
+          image={"/images/about-us-3.png"}
+        />
       </div>
     </div>
   );
--- src/component/aboutUs/AboutUsItem.tsx
+++ src/component/aboutUs/AboutUsItem.tsx
@@ -1,42 +1,53 @@
-'use client'
+"use client";
 import React, { useRef } from "react";
 import Image from "next/image";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { cn } from "@/lib/utils";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const AboutUsItem = () => {
+const AboutUsItem = ({ description, direction, title, image, className }: {
+  title: string,
+  description: string,
+  image: string,
+  direction: "left" | "right",
+  className?: string
+}) => {
 
   const timeline = useRef<any>();
+  const container = useRef<HTMLDivElement>(null);
 
   useGSAP(() => {
-    timeline.current = gsap.timeline().to("#this-text" , {
-      bottom: "unset" , top: 0,
-    })
+    gsap.matchMedia().add("(min-width: 1024px)", () => {
+      timeline.current = gsap.timeline().to(".about-us-text", {
+        top: 500
+      });
 
-    ScrollTrigger.create({
-      animation: timeline.current,
-      trigger: ".item-image",
-      start: "top 0%",
-      end: "bottom 0%",
-      scrub: 2,
-      pin: true
-    })
-  })
+      ScrollTrigger.create({
+        animation: timeline.current,
+        trigger: ".item-image",
+        start: "top 0%",
+        end: "bottom 0%",
+        scrub: 2,
+        pin: true
+      });
+    });
+  }, { scope: container });
 
   return (
-    <div className={'w-full relative grid grid-cols-12 gap-8'}>
-      <div className="col-span-5 relative h-full w-full">
-        <p className={'absolute bottom-0 left-0'} id={'this-text'}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam amet aspernatur assumenda atque aut
-          deserunt distinctio eaque eius exercitationem facere fuga illo optio porro reiciendis, sapiente sequi suscipit
-          totam.
-        </p>
+    <div ref={container} className={cn("w-full relative grid grid-cols-12 gap-8", className)}>
+      <div className={`col-span-12 lg:col-span-5 relative h-full w-full ${direction == "left" ? "lg:order-1" : "lg:order-2"}`}>
+        <div className={"about-us-text lg:absolute lg:bottom-0 lg:left-0 w-full h-fit flex flex-col gap-8"}>
+          <h2 className={"text-3xl font-bold"}>{title}</h2>
+          <p className={"text-xl font-normal"}>
+            {description}
+          </p>
+        </div>
       </div>
-      <div className="col-span-7 max-w-[700px] mx-auto relative pb-[90vh] w-full rounded-3xl overflow-hidden item-image">
-        <Image src={'/images/about-us-1.png'} alt={'about us'} fill />
+      <div className={`col-span-12 lg:col-span-7 lg:max-w-[700px] mx-auto relative pb-[90vh] w-full rounded-3xl overflow-hidden item-image ${direction == "left" ? "lg:order-2" : "lg:order-1"}`}>
+        <Image src={image} alt={title} fill />
       </div>
     </div>
   );
