9cd9b0e4b780d440400e36aeee1fbc6baf9310d6
MohammadShahheydar <m.shahheydar110>
2024-06-25 15:20:38 +0330

    subtitle font & color

--- src/component/landing/subtitle/SubtitleText.tsx
+++ src/component/landing/subtitle/SubtitleText.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SubtitleText = ({title}: {title: string}) => {
   return (
-    <p className={'text-2xl sm:text-3xl md:text-5xl'}>{title}</p>
+    <p className={'text-2xl sm:text-3xl md:text-5xl font-[playfair] text-[#D6E6FC]'}>{title}</p>
   );
 };
 
