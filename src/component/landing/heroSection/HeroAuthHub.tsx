c99cd425e37a1d75484df78b8461b161c0d887d4
h.ahari <hsoleimaneahary@gmail.com>
2024-06-27 20:50:09 +0330

    fix tensurf hub button

--- src/component/landing/heroSection/HeroAuthHub.tsx
+++ src/component/landing/heroSection/HeroAuthHub.tsx
@@ -2,18 +2,22 @@
 
 import { Button } from "@/components/ui/button";
 import { isLoggedIn } from "@/helpers/auth";
+import { useRouter } from "next/navigation";
 
 export const HeroAuthHub = () => {
+  const router = useRouter();
   return (
     <div>
-      <a
-        href={isLoggedIn() ? process.env.NEXT_PUBLIC_HUB_URL : "/signin"}
-        target={isLoggedIn() ? "_blank" : '_self'}
+      <Button
+        className="h-16 bg-primary font-bold text-lg"
+        onClick={() =>
+          isLoggedIn()
+            ? router.push(process.env.NEXT_PUBLIC_HUB_URL as string)
+            : router.push("/signin")
+        }
       >
-        <Button className="h-16 bg-primary font-bold text-lg">
-          Go to TenSurf Hub
-        </Button>
-      </a>
+        Go to TenSurf Hub
+      </Button>
     </div>
   );
 };
