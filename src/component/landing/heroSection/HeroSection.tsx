e15e6b53c5543cf4b80bfe68e7083f1cc2366a16
MohammadShahheydar <m.shahheydar110>
2024-06-27 17:40:14 +0330

    update hero section

--- src/component/landing/heroSection/HeroSection.tsx
+++ src/component/landing/heroSection/HeroSection.tsx
@@ -4,27 +4,23 @@ import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 import { isLoggedIn } from "@/helpers/auth";
-import { HeroAuthHub } from "./HeroAuthHub";
 
 
 const animateLights = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-export const HeroSection = () => {
+export const HeroSection = ({title , description}: {title: string , description: string}) => {
 
   return (
     <div
-      className={`relative w-full flex flex-col items-center overflow-hidden h-screen`}
+      className={`w-full flex flex-col items-center overflow-hidden h-screen`}
     >
       <div className="flex items-center flex-col justify-center gap-14 relative z-[9] max-w-2xl min-h-screen">
         <div className="flex items-center flex-col justify-center gap-14 ">
           <h1 className={"text-5xl sm:text-7xl text-center font-medium text-[#F8F9FA]"}>
-            Next Generation of Trading Platforms
+            {title}
           </h1>
           <p className={"text-lg sm:text-xl font-normal text-center text-[#DEE2E6]"}>
-            At TenSurf, we are committed to revolutionizing trading and
-            democratizing algorithmic trading. Our mission is to harness the power
-            of Generative AI and Data Science to enable smart trading decisions to
-            surf financial markets.
+            {description}
           </p>
         </div>
         <Link
@@ -44,7 +40,7 @@ export const HeroSection = () => {
       {/*</a>*/}
 
       <div
-        className="absolute z-0 h-full w-full items-stretch flex-wrap gap-y-[20000px] justify-between flex gap-[200px] max-w-[90%]">
+        className="absolute z-0 h-full top-0 w-full items-stretch flex-wrap gap-y-[20000px] justify-between flex gap-[200px] max-w-[90%]">
         {animateLights.map((light, index) => (
           <AnimateLight key={index} index={index} />
         ))}
